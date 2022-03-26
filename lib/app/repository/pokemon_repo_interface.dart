7acfeef223a0606d28fab575725741a4ab0ee616
Matheus Miranda <matheus.sm86@gmail.com>
2022-03-26 17:37:56 -0300

    [FEAT] - Create Pokemon Repository Interface

---
+++ lib/app/repository/pokemon_repo_interface.dart
@@ -0,0 +1,6 @@
+import 'package:pokemon/app/domain/pokemon.dart';
+
+abstract class PokemonRepoInterface {
+  void addToFavoritesList(Pokemon pokemon);
+  void addToObtainedList(Pokemon pokemon);
+}
